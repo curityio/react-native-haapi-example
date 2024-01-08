479af3ba2e350870ce039a0d4109e1bff58eba83
Daniel Lindau <d.lindau@gmail.com>
2024-01-09 00:52:59 +0100

    Config for attestation fallback

--- configuration.android.jsx
+++ configuration.android.jsx
@@ -6,6 +6,9 @@ const HaapiConfiguration = {
     "tokenEndpointUri": "https://dlindau.ngrok.io/dev/oauth/token",
     "authorizationEndpointUri": "https://dlindau.ngrok.io/dev/oauth/authorize",
     "revocationEndpointUri": "https://dlindau.ngrok.io/revoke",
-    "scope": "openid profile"
+    "scope": "openid profile",
+    "registrationEndpointUri": "https://dlindau.ngrok.io/oauth-dev/oauth-registration",
+    "fallback_template_id": "react-native-fallback",
+    "registration_secret": "my-good-secret"
 }
 export default HaapiConfiguration
