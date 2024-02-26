c8d5cc1fa18d9a7e23c0e093082fe1149d7c918c
Daniel Lindau <d.lindau@gmail.com>
2024-02-26 13:35:15 +0100

    Add ios config

--- components/HaapiModule.jsx
+++ components/HaapiModule.jsx
@@ -1,5 +1,5 @@
 import {NativeModules} from "react-native";
-import HaapiConfiguration from "../configuration.android";
+import HaapiConfiguration from "../configuration";
 
 const {HaapiModule} = NativeModules;
 HaapiModule.load(HaapiConfiguration)
---
+++ configuration.ios.jsx
@@ -0,0 +1,11 @@
+const HaapiConfiguration = {
+    "appRedirect": "app:start",
+    "clientId": "react-dev-ios-client",
+    "baseUri": "https://dlindau.ngrok.io",
+    "tokenEndpointUri": "https://dlindau.ngrok.io/dev/oauth/token",
+    "authorizationEndpointUri": "https://dlindau.ngrok.io/dev/oauth/authorize",
+    "revocationEndpointUri": "https://dlindau.ngrok.io/revoke",
+    "acrValues": "urn:se:curity:authentication:username:username",
+    "scope": "openid profile",
+}
+export default HaapiConfiguration
