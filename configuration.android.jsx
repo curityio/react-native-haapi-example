61d826d9b65b7e03f7d339b741d963eac71b426c
gary.archer <gary.archer@curity.io>
2025-02-24 11:24:25 +0100

    Put back initial configuration

---
+++ configuration.android.jsx
@@ -0,0 +1,17 @@
+const HaapiConfiguration = {
+    "appRedirect": "app:start",
+    "keyStoreAlias": "haapi-react-native",
+    "clientId": "react-android",
+    "baseUri": "https://dlindau.ngrok.io",
+    "tokenEndpointUri": "https://dlindau.ngrok.io/dev/oauth/token",
+    "authorizationEndpointUri": "https://dlindau.ngrok.io/dev/oauth/authorize",
+    "revocationEndpointUri": "https://dlindau.ngrok.io/revoke",
+    // "registrationEndpointUri": "https://dlindau.ngrok.io/register",
+    // "fallback_template_id": "react-android-template",
+    // "registration_secret": "Password1",
+    "validateTlsCertificate": true,
+    "scope": "openid profile",
+    "extraRequestParameters": {"ui_locales": "sv"},
+    "extraHttpHeaders": {"my-good-header": "foo-bar"}
+}
+export default HaapiConfiguration
---
+++ configuration.ios.jsx
@@ -0,0 +1,12 @@
+const HaapiConfiguration = {
+    "appRedirect": "app:start",
+    "clientId": "react-ios-client",
+    "baseUri": "https://dlindau.ngrok.io",
+    "tokenEndpointUri": "https://dlindau.ngrok.io/dev/oauth/token",
+    "authorizationEndpointUri": "https://dlindau.ngrok.io/dev/oauth/authorize",
+    "revocationEndpointUri": "https://dlindau.ngrok.io/revoke",
+    "validateTlsCertificate": false,
+    "acrValues": "",
+    "scope": "openid",
+}
+export default HaapiConfiguration
